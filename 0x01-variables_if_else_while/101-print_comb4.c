6b342f43bede42f02c5465389b19ef4e9875b070
Oni John <onijohnoluwaseun@gmail.com>
2022-03-15 05:41:07 +0100

    Task DonE

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,24 +7,28 @@
 */
 int main(void)
 {
-  int tens;
-  int ones;
+	int i, j, k, l;
 
-  for (tens = 0; tens <= 9; tens++)
-    {
-      for (ones = tens + 1; ones <= 9; ones++)
+	for (i = 0; i < 1000; i++)
 	{
-	  putchar(tens + '0');
-	  putchar(ones + '0');
+		j = i / 100; /* hundreds */
+		k = (i / 10) % 10; /* tens */
+		l = i % 10; /* singles */
 
-	  if (tens < 8)
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
+		if (j < k && k < l)
+		{
+			putchar(j + '0');
+			putchar(k + '0');
+			putchar(l + '0');
+
+			if (i < 700)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
 	}
-    }
-  putchar('\n');
+	putchar('\n');
 
-  return (0);
+	return (0);
 }
