7760a61c3c48b931540b997628c93e1fc3611d5d
Oni John <onijohnoluwaseun@gmail.com>
2022-03-18 05:58:58 +0100

    TASK DONE

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_number - prints an integer
- * @n: number to be printed
- */
+* print_number - prints # using _putchar function
+* @n: the integer to print
+*
+* Return: void
+*/
 
 void print_number(int n)
 {
