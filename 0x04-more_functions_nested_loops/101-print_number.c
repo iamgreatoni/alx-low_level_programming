f6b63ea5d5a595ba6d2898e77fa1d9f95b0b5afe
Oni John <onijohnoluwaseun@gmail.com>
2022-03-18 05:57:24 +0100

    TASK DONE

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,28 +8,31 @@
 
 void print_number(int n)
 {
-int copy, nth, size = 1, ones = n % 10;
+	int num;
+	int factor = 0;
+	int i;
+	int digit;
+	int last_digit;
 
-n /= 10;
-copy = n;
-if (ones < 0)
-{
-ones *= -1, copy *= -1, n *= -1;
-_putchar('-');
-}
-if (copy > 0)
-{
-while (copy / 10 != 0)
-{
-copy /= 10, size *= 10;
-}
-while (size > 0)
-{
-nth = n / size;
-_putchar('0' + nth);
-n -= nth * size;
-size /= 10;
-}
-}
-_putchar('0' + ones);
+	digit = digit_num(n);
+	if (digit == 0)
+		_putchar('0' + n);
+	else if (digit > 0)
+	{
+		if (n < 0)
+			_putchar('-');
+		for (i = digit - 1; i >= 1; i--)
+		{
+			factor = ten_to_power(i);
+			num = (n / factor) % 10;
+			if (num < 0)
+				num = -num;
+			_putchar('0' + num);
+		}
+		if (n < 0)
+			last_digit = -(n % 10);
+		else
+			last_digit = n % 10;
+		_putchar('0' + last_digit);
+	}
 }
